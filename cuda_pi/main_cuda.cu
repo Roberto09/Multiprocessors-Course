2a65be4d1f276f551b5b9610b44e088680d60e7e
roberto09 <roberto.gt1509@gmail.com>
2021-05-24 14:45:00 -0500

    Fix issues with main_cuda.cu

--- cuda_pi/main_cuda.cu
+++ cuda_pi/main_cuda.cu
@@ -43,6 +43,6 @@ int main() {
     float acum = 0;
     for(int i = 0; i < ttl_threads; i++) acum += h_tmp_storage[i];
 
-    printf("Result = %20.18lf (%ld)\n", acum, get_millisec(start, eend));
+    printf("Result = %20.18lf (%ld)\n", acum);
     return 0;
 }
